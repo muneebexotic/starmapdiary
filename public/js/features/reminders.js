df19e9ae1a40c575a1b35bf99059da2c06351c92
muneebexotic <muneebusman112233@gmail.com>
2026-04-09 18:04:00 +0500

    updated time stamps for notifications

--- public/js/features/reminders.js
+++ public/js/features/reminders.js
@@ -1,4 +1,4 @@
-const DEFAULT_REMINDER_TIMES = ["09:00:00", "14:00:00", "20:00:00"];
+const DEFAULT_REMINDER_TIMES = ["01:00:00", "13:00:00", "19:00:00", "23:00:00"];
 
 function isPushSupported() {
   return "serviceWorker" in navigator && "PushManager" in window && "Notification" in window;
--- src/services/reminders/constants.js
+++ src/services/reminders/constants.js
@@ -1,4 +1,4 @@
-const DEFAULT_REMINDER_TIMES = ["09:00:00", "14:00:00", "20:00:00"];
+const DEFAULT_REMINDER_TIMES = ["01:00:00", "13:00:00", "19:00:00", "23:00:00"];
 
 module.exports = {
   DEFAULT_REMINDER_TIMES
